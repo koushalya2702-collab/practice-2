cff6197f1c1457e7683fa229ae45b0fefb03efd5
koushalya2702-collab <koushalya2702@gmail.com>
2026-04-15 22:19:27 +0530

    add phython6.py

--- phython6.py
+++ phython6.py
@@ -1,55 +1,55 @@
-# #for loop
-# cities = ["Bengaluru", "Mysuru", "Hubballi", "Mangaluru"]
-# for city in cities:
-#     print(city)
+#for loop
+cities = ["Bengaluru", "Mysuru", "Hubballi", "Mangaluru"]
+for city in cities:
+    print(city)
 
-# #Using range() with for Loops
-# for i in range(1,11):
-#     print(i)
-
-
-# #Counting by 2s from 1 to 10
-# for i in range(1,11,2):
-#     print(i)
+#Using range() with for Loops
+for i in range(1,11):
+    print(i)
 
 
-# #Looping Over Strings
-# name = "Karnataka"
-# for letter in name:
-#     print(letter)
+#Counting by 2s from 1 to 10
+for i in range(1,11,2):
+    print(i)
 
 
-# #Nested for Loops
-# for i in range(1,6):
-#     for j in range(1,6):
-#         print(f"{i} x {j} = {i*j}")  
-#     print() 
+#Looping Over Strings
+name = "Karnataka"
+for letter in name:
+    print(letter)
 
 
-# # Using break in a for Loop
-# cities = ["Bengaluru", "Mysuru", "Hubballi", "Mangaluru"]
-# for city in cities:
-#     if city == "Mysuru":
-#         print(f"found {city}")
-#         break
-#     print(city)
-
-# # Using continue in a for Loop
-# cities = ["Bengaluru", "Mysuru", "Hubballi", "Mangaluru"]
-# for city in cities:
-#     if city == "Mysuru":
-#         continue
-#     print(city)
-
-# # Looping Through a List with enumerate()
-# cities = ["Bengaluru", "Mysuru", "Hubballi", "Mangaluru"]
-# for index, city in enumerate(cities):
-#     print(f" city {index+1}:{city}")
+#Nested for Loops
+for i in range(1,6):
+    for j in range(1,6):
+        print(f"{i} x {j} = {i*j}")  
+    print() 
 
 
-# #Multiples of 3:
-# for i in range(0,30,3):
-#     print(i)
+# Using break in a for Loop
+cities = ["Bengaluru", "Mysuru", "Hubballi", "Mangaluru"]
+for city in cities:
+    if city == "Mysuru":
+        print(f"found {city}")
+        break
+    print(city)
+
+# Using continue in a for Loop
+cities = ["Bengaluru", "Mysuru", "Hubballi", "Mangaluru"]
+for city in cities:
+    if city == "Mysuru":
+        continue
+    print(city)
+
+# Looping Through a List with enumerate()
+cities = ["Bengaluru", "Mysuru", "Hubballi", "Mangaluru"]
+for index, city in enumerate(cities):
+    print(f" city {index+1}:{city}")
+
+
+#Multiples of 3:
+for i in range(0,30,3):
+    print(i)
 
 #Sum of First 10 Numbers:
 sum=0
@@ -61,7 +61,7 @@ print(sum)
 #Count Vowels in a String:
 count = 0
 vowel = "aeiou"
-str = input("Enter a string: ")
+str = input("Enter a string: ").lower()
 for letter in str:
     if letter in vowel:
         count += 1
